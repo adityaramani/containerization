b70861e677bb97720721da316d921ab66b0fe89c
Elijah Wright <git@elijahs.space>
2025-06-18 08:25:10 -0700

    fix formatting again

--- vminitd/Sources/vminitd/TerminalIO.swift
+++ vminitd/Sources/vminitd/TerminalIO.swift
@@ -138,7 +138,7 @@ final class TerminalIO: ManagedProcess.IO & Sendable {
                 case .again:
                     // We read all we could, exit.
                     if mask.isHangup {
-                    cleanupRelay()
+                        cleanupRelay()
                     }
                     return
                 default:
